34f23fb33e6a6bbf9d1d86a3a2627b5ac9660383
Gil Mandler <gil.mandler@gmail.com>
2015-03-03 09:08:14 -0500

    refactor builder for simpler use, files_worker now has all basic functionality.

--- builder.py
+++ builder.py
@@ -5,15 +5,15 @@ from apiclient.discovery import build
 from auth import Authorize
 
 
-class BuildService:
+class BuildService(object):
+    """build the drive service with an authorized http object"""
+
     def __init__(self):
-        """ Get credentials and create a http authorize object"""
         self.credentials = Authorize().get_credentials()
         self.http_auth = self.credentials.authorize(Http())
+        self.service = build('drive', 'v2', http=self.http_auth)
 
-    def build_drive_service(self):
-        """Build an authenticated drive service object"""
-        return build('drive', 'v2', http=self.http_auth)
+
 
 
 
--- files_worker.py
+++ files_worker.py
@@ -1,20 +1,12 @@
 from os import path
-from pprint import pprint
+import json
 
 from builder import BuildService
 
-class DriveFiles:
+class DriveFiles(object):
     def __init__(self):
-        # build the drive service with an http object
-        self.drive_service = BuildService().build_drive_service()
-
+        self.drive_service = BuildService().service
         self.drive_files_service = self.drive_service.files()
-
-        self.payload_types = (
-        "mimeType = 'application/vnd.google-apps.document'"
-        "or mimeType = 'application/vnd.google-apps.presentation'"
-        "or mimeType = 'application/vnd.google-apps.spreadsheet'"
-        )
         self.export_type_dict = {
             'application/vnd.google-apps.document':
                 ('application/vnd.openxmlformats-officedocument.wordprocessingml'
@@ -23,41 +15,81 @@ class DriveFiles:
                 ('application/vnd.openxmlformats-officedocument.spreadsheetml'
                 '.sheet','xlsx')
         }
+        self.STORAGE_PATH = "file_store"
+        self.JSON_STORAGE = path.join(self.STORAGE_PATH, 'meta.json')
+        self.file_list = {}
 
-    def get_file_list(self):
-        """returns a list of files and their meta data which match the payload"""
-        payload = self.payload_types
-        return self.drive_files_service.list(maxResults=5, q=payload).execute()
+    def _get_file_list(self):
+        #TODO DO NOT GET FILES FROM TRASH
+        """sets file_list attr as a dict of file meta data which match
+        payload and their meta  data as a dict"""
 
-    def get_export_links(self):
-        """adds each file drive id as a key, and title and exportLink as
-        values to the file_meta dict """
-        file_meta = {}
-        files_list = self.get_file_list()
-        for f in files_list['items']:
-            links = f['exportLinks']
-            export_type = self.export_type_dict[f['mimeType']]
-            export_link = links[export_type[0]]
-            extension = export_type[1]
-            file_meta[f['id']] = (f['title'], export_link, extension)
-        return file_meta
+        payload_types = (
+        "mimeType = 'application/vnd.google-apps.document'"
+        "or mimeType = 'application/vnd.google-apps.presentation'"
+        "or mimeType = 'application/vnd.google-apps.spreadsheet'"
+        )
+        payload = {"q": payload_types}
+        items = self.drive_files_service.list(**payload).execute()['items']
+
+        self.file_list = {item.pop('id'): item for item in items}
+
+    def _store_file_list(self):
+        with open(self.JSON_STORAGE, "wb") as f:
+            json.dump(self.file_list, f)
+
+    def file_list_from_storage(self):
+        with open(self.JSON_STORAGE, 'r') as f:
+            return json.load(f)
+
+    def filter_download_list(self):
+        # TODO handle deleted files they will cause a key error
+        try:
+            stored_file_meta = self.file_list_from_storage()
+        except:
+            raise
+        return {k: v for k, v in stored_file_meta if v['modifiedDate'] !=
+                self.file_list[k]['modifiedDate']}
+
+    def get_export_links(self, file_meta):
+        v = file_meta
+        links = v['exportLinks']
+        export_type = self.export_type_dict[v['mimeType']]
+        export_link = links[export_type[0]]
+        extension = export_type[1]
+
+        # need to map unicode  to none if using translate
+        replace_me = u'/|/'
+        trans_table = {ord(c): u'_' for c in replace_me}
+        return v['title'].translate(trans_table), export_link, extension
 
     def download_files(self):
-        file_list = self.get_export_links()# change this to a function that
-        # contains the download list based on db check
-        for title, furl, extension in file_list.values():
-            response, content = self.drive_service._http.request(furl)
+        self._get_file_list()
+        export_list = self.file_list
+
+        if path.isfile(self.JSON_STORAGE):
+            export_list = self.filter_download_list()
+
+        self._store_file_list()
+
+        for item in export_list.values():
+            title, eurl, extension = self.get_export_links(item)
+            response, content = self.drive_service._http.request(eurl)
+
             print response.status
-            file_name = path.join('file_store', title + "." + extension)
-            with open(file_name,'wb' ) as f:
+
+            file_name = path.join(self.STORAGE_PATH, title + "." + extension)
+            with open(file_name, 'wb') as f:
                 f.write(content)
 
-DriveFiles().download_files()
-#pprint((drive_files))
+
+#TODO test this when you change a file
 
 
-#TODO store file id and modified date and download url in a db
-#TODO check against local db if they have changed since last download
-#TODO if they changed add them to download list
+DF = DriveFiles()
+DF.download_files()
+#print len(DriveFiles().get_export_links())
+#DF.store_file_list()
+#pprint(DF.file_list_from_storage())
 
-#TODO Download files listed in download list in proper ms office format
+
