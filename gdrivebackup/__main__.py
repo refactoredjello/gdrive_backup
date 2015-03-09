6c7e88544957c28dc81b85abec11cdbfd864893f
Gil Mandler <gil.mandler@gmail.com>
2015-03-09 00:10:36 -0400

    Merged from dev

---
+++ gdrivebackup/__main__.py
@@ -0,0 +1,25 @@
+#TODO FIX LOGGER!
+#TODO work out why there are duplicates on first download
+#TODO save locally in a folder that matches the drive folder
+#TODO add local file locations to data storage
+#TODO update meta.json if new files downloaded
+#TODO move file meta storage to sql database
+
+import drive_service
+from os import path
+# from datetime import datetime
+# import logging
+#
+#
+#
+# #_format = '%(levelname)s %(asctime): %(message)s'
+# log_name = 'log' + datetime.now().strftime('%Y_%m_%d-%H-%M_%S') + '.txt'
+# log = logging.getLogger(__name__)
+# log.basicConfig(filename=log_name, level=logging.DEBUG,)
+
+
+if __name__ == '__main__':
+    DF = drive_service.DriveServiceWorker()
+    print DF.JSON_STORAGE
+    print path.exists(DF.JSON_STORAGE)
+    DF.download_files()
